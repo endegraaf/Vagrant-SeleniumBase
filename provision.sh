64775816bbf8d3ec0caeb791b9b77df0cdd92347
Eric de Graaf <endegraaf@gmail.com>
2017-07-04 08:43:55 +0200

    Merge back into one file

--- provision.sh
+++ provision.sh
@@ -1,9 +1,60 @@
 #!/usr/bin/env bash
 set -e
 export HOME=/home/vagrant
-echo provisioning part deux ...
 
-echo JAVA8
+echo provisioning...
+sudo apt-get update
+sudo apt-get install -y build-essential
+sudo apt-get install -y libmysqlclient-dev
+sudo apt-get install -y git
+sudo apt-get install -y firefox-esr            #browsers
+#sudo apt-get install -y google-chrome-stable   #browsers
+
+#mysql password
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password vagrant'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password vagrant'
+sudo apt-get install -y --force-yes mysql-server
+
+
+# Gecko driver
+cd /home/vagrant
+if [[ -d ./gecko ]]; then
+   echo Gecko directory already exists
+else    
+    if [[ ! -d ./gecko ]]; then
+        mkdir gecko
+    fi
+    cd gecko
+    wget https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-linux64.tar.gz
+    tar xvzf geckodriver-v0.16.1-linux64.tar.gz
+    sudo cp geckodriver /usr/local/bin
+fi
+
+#Chromedriver
+cd /home/vagrant
+
+if [[ ./chromedriver ]]; then
+   echo Chromedriver directory already exists
+else    
+    mkdir chromedriver
+    cd chromedriver
+    wget https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip
+    unzip chromedriver_linux64.zip
+    sudo cp chromedriver /usr/local/bin
+fi
+
+
+if [[ ! -d ./SimpleBlog ]]; then
+    git clone https://github.com/endegraaf/SimpleBlog.git
+else
+    cd SimpleBlog
+    git pull
+    cd ..
+fi
+
+sudo chown -R vagrant SimpleBlog
+
+
 
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
@@ -12,7 +63,7 @@ echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-#sudo apt-get update
+sudo apt-get update
 
 sudo dpkg --configure -a
 
