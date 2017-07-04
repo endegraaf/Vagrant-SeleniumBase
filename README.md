e7998fa8be6b966597d72ef3c449733be826d4a7
Eric de Graaf <endegraaf@gmail.com>
2017-07-04 10:42:25 +0200

    Fix vagrant provisioning

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Vagrant-SimpleBlog
-Vagrant environment for SimpleBlog demo application (CTA)
+A Vagrant based virtual environment for the SimpleBlog demo application (Alten IT - Core team Agile).
+
+# Instructions for getting started with Vagrant
+## Prerequisites
+- Oracle VirtualBox
+- Vagrant https://www.vagrantup.com/downloads.html
+
+## Start the Vagrant Virtual Machine
+- Clone this repo
+- Go into the local repository from the step above
+- vagrant up --provider=virtualbox
+- Now wait for the process to complete
+
+## Open the Virtual machine (debian 8)
+- Firefox should be installed, navigate to http://localhost:8088/blog/index.xhtml and see the blog.
+- use admin/admin to login to the blog
+
+## Support 
+- In case of any questions or concerns regarding this VM please contact eric.de.graaf@alten.nl
--- provision.sh
+++ provision.sh
@@ -3,46 +3,26 @@ set -e
 export HOME=/home/vagrant
 
 echo provisioning...
+
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 sudo apt-get update
 sudo apt-get install -y build-essential
 sudo apt-get install -y libmysqlclient-dev
 sudo apt-get install -y git
-sudo apt-get install -y firefox-esr            #browsers
-#sudo apt-get install -y google-chrome-stable   #browsers
+sudo apt-get install -y firefox-esr #browsers #sudo apt-get install -y google-chrome-stable
+sudo apt-get install -y oracle-java8-installer
+sudo apt-get install -y oracle-java8-set-default
+sudo apt-get install -y maven
 
 #mysql password
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password vagrant'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password vagrant'
 sudo apt-get install -y --force-yes mysql-server
-
-
-# Gecko driver
-cd /home/vagrant
-if [[ -d ./gecko ]]; then
-   echo Gecko directory already exists
-else    
-    if [[ ! -d ./gecko ]]; then
-        mkdir gecko
-    fi
-    cd gecko
-    wget https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-linux64.tar.gz
-    tar xvzf geckodriver-v0.16.1-linux64.tar.gz
-    sudo cp geckodriver /usr/local/bin
-fi
-
-#Chromedriver
-cd /home/vagrant
-
-if [[ ./chromedriver ]]; then
-   echo Chromedriver directory already exists
-else    
-    mkdir chromedriver
-    cd chromedriver
-    wget https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip
-    unzip chromedriver_linux64.zip
-    sudo cp chromedriver /usr/local/bin
-fi
-
+sudo dpkg --configure -a
 
 if [[ ! -d ./SimpleBlog ]]; then
     git clone https://github.com/endegraaf/SimpleBlog.git
@@ -52,25 +32,7 @@ else
     cd ..
 fi
 
-sudo chown -R vagrant SimpleBlog
-
-
-
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
-
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-sudo apt-get update
-
-sudo dpkg --configure -a
-
-sudo apt-get -y install oracle-java8-installer
-sudo apt-get -y install oracle-java8-set-default
-sudo apt-get -y install  maven
-
+sudo chown -R vagrant:vagrant /home/vagrant
 
 # Flyway
 echo Install Flyway database migration
@@ -86,19 +48,18 @@ export PATH=$PATH:$HOME/Flyway/flyway-4.2.0
 cd $HOME
 git clone https://github.com/domdorn/spring-security-facelets-taglib.git
 cd spring-security-facelets-taglib
-mvn clean install 
+mvn clean install -DskipTests #skip tests because of the embedded selenium one.
 
-
-# SimpleBLog code
+cd $HOME
+# SimpleBlog code
 if [[ ! -d ./SimpleBlog ]]; then
     git clone https://github.com/endegraaf/SimpleBlog.git
-else
-    cd SimpleBlog
-    git pull
 fi
 
-cd $HOME && sudo chown -R vagrant:vagrant $HOME
+cd SimpleBlog
+git pull
 
+cd $HOME && sudo chown -R vagrant:vagrant $HOME
 
 
 # Mysql Local database
@@ -111,10 +72,41 @@ mysql --user=root --password=vagrant --execute="GRANT ALL PRIVILEGES ON blog.* T
 cd $HOME/SimpleBlog
 
 
-flyway  -baselineOnMigrate=false -url=jdbc:mysql://localhost/ -schemas=blog -user=bloguser -password=blogpassword -locations=filesystem:src/main/resources/db/migration/ migrate
+flyway  -baselineOnMigrate=true -url=jdbc:mysql://localhost/ -schemas=blog -user=bloguser -password=blogpassword -locations=filesystem:src/main/resources/db/migration/ migrate
 
 mvn clean install
 mvn tomcat7:run-war
 
 
+
+
+
+
+
+## Gecko driver
+#cd /home/vagrant
+#if [[ -d ./gecko ]]; then
+#   echo Gecko directory already exists
+#else    
+#    if [[ ! -d ./gecko ]]; then
+#        mkdir gecko
+#    fi
+#   cd gecko
+#   wget https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-linux64.tar.gz
+#    tar xvzf geckodriver-v0.16.1-linux64.tar.gz
+#    sudo cp geckodriver /usr/local/bin
+#fi
+#
+##Chromedriver
+#cd /home/vagrant
+#
+#if [[ ./chromedriver ]]; then
+#   echo Chromedriver directory already exists
+#else    
+#    mkdir chromedriver
+#    cd chromedriver
+#    wget https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip
+#    unzip chromedriver_linux64.zip
+#    sudo cp chromedriver /usr/local/bin
+#fi
 
